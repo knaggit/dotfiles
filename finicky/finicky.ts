6cefae779eb0c147c62d3f9a1399ed009c5566c8
Marjan Grabowski <hello@knaggit.de>
2025-12-08 12:45:52 +0100

    reconfigure finicky

---
+++ finicky/finicky.ts
@@ -0,0 +1,35 @@
+// Finicky on GitHub: https://github.com/johnste/finicky
+// Search for integration on GitHub: https://github.com/search?q=filename%3A.finicky.js+Spotify&type=Code
+
+import type { FinickyConfig } from "/Applications/Finicky.app/Contents/Resources/finicky.d.ts";
+
+export default {
+  defaultBrowser: "Arc",
+  options: {
+    checkForUpdates: false,
+    hideIcon: true,
+    logRequests: false,
+  },
+  handlers: [
+    {
+      match: "meet.google.com/*",
+      browser: "Arc",
+    },
+    {
+      match: "open.spotify.com/*",
+      browser: "Spotify",
+    },
+    {
+      match: "*.zoom.us/j/*",
+      browser: "us.zoom.xos",
+    },
+    {
+      match: /notion\.so\/.*[a-f0-9]{32}/,
+      browser: "Notion",
+    },
+    {
+      match: "www.figma.com/file/*",
+      browser: "Figma",
+    },
+  ],
+} satisfies FinickyConfig;
